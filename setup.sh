2406fc6288ba4f2c3e32fc29572ae06d4f9a9e22
Yui Tomo <yuitomo@MacBookProYuiTomoNCNP.local>
2021-01-04 14:09:08 +0900

    :up: update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,15 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo "default:" > config/config.yaml
 echo "  project_path: '${SCRIPT_DIR}/R'" >> config/config.yaml
+#cat ./R/org.R | sed -e "s|project_path|${SCRIPT_DIR}/R|g" > ./R/main.R
+echo "#!/usr/bin/env Rscript
+# -*- coding: utf-8 -*-
+
+setwd('${SCRIPT_DIR}/R')
+source('./functions.R')
+
+
+if (sys.nframe() == 0){
+  print('Hello, World!')
+  getwd()
+}" > ./R/main.R
